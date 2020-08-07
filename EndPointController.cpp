0c208e6be614a34ab46d8ef5b923949863596e44
tomxue <tomxue@outlook.com>
2020-08-07 17:38:33 +0800

    重构代码；PlatformToolset版本从v120改为v142

--- EndPointController.cpp
+++ EndPointController.cpp
@@ -10,12 +10,12 @@
 #include "Functiondiscoverykeys_devpkey.h"
 
 HRESULT SetDefaultAudioPlaybackDevice(LPCWSTR devID)
-{	
-	IPolicyConfigVista *pPolicyConfig;
+{
+	IPolicyConfigVista* pPolicyConfig;
 	ERole reserved = eConsole;
 
-    HRESULT hr = CoCreateInstance(__uuidof(CPolicyConfigVistaClient), 
-		NULL, CLSCTX_ALL, __uuidof(IPolicyConfigVista), (LPVOID *)&pPolicyConfig);
+	HRESULT hr = CoCreateInstance(__uuidof(CPolicyConfigVistaClient),
+		NULL, CLSCTX_ALL, __uuidof(IPolicyConfigVista), (LPVOID*)&pPolicyConfig);
 	if (SUCCEEDED(hr))
 	{
 		hr = pPolicyConfig->SetDefaultEndpoint(devID, reserved);
@@ -27,88 +27,97 @@ HRESULT SetDefaultAudioPlaybackDevice(LPCWSTR devID)
 // EndPointController.exe [NewDefaultDeviceID]
 int _tmain(int argc, _TCHAR* argv[])
 {
+	int errorRet = -2;
 	// read the command line option, -1 indicates list devices.
 	int option = -1;
 	LPWSTR wstrOption = NULL;
 	size_t strlenOption = 0;
-	if(argc == 2) {
+	if (argc == 2) {
 		wstrOption = argv[1];
 		strlenOption = wcslen(argv[1]);
 		int i = 0;
-		while(wstrOption[i] != NULL && iswdigit(wstrOption[i]))
+		while (wstrOption[i] != NULL && iswdigit(wstrOption[i]))
 			i++;
-		if(wstrOption[i] == NULL) {
+		if (wstrOption[i] == NULL) {
 			// argument is a digit
 			option = _wtoi(argv[1]);
 		}
 	}
 
 	HRESULT hr = CoInitialize(NULL);
-	if (SUCCEEDED(hr))
+	if (!SUCCEEDED(hr))
+		return errorRet;
+
+	IMMDeviceEnumerator* pEnum = NULL;
+	// Create a multimedia device enumerator.
+	hr = CoCreateInstance(__uuidof(MMDeviceEnumerator), NULL,
+		CLSCTX_ALL, __uuidof(IMMDeviceEnumerator), (void**)&pEnum);
+
+	if (!SUCCEEDED(hr))
+		return errorRet;
+
+	IMMDeviceCollection* pDevices;
+	// Enumerate the output devices.
+	hr = pEnum->EnumAudioEndpoints(eRender, DEVICE_STATE_ACTIVE, &pDevices);
+
+	if (!SUCCEEDED(hr))
+		return errorRet;
+
+	UINT count;
+	pDevices->GetCount(&count);
+
+	if (!SUCCEEDED(hr))
+		return errorRet;
+
+	for (UINT i = 0; i < count; i++)
 	{
-		IMMDeviceEnumerator *pEnum = NULL;
-		// Create a multimedia device enumerator.
-		hr = CoCreateInstance(__uuidof(MMDeviceEnumerator), NULL,
-			CLSCTX_ALL, __uuidof(IMMDeviceEnumerator), (void**)&pEnum);
-		if (SUCCEEDED(hr))
+		IMMDevice* pDevice;
+		hr = pDevices->Item(i, &pDevice);
+
+		if (!SUCCEEDED(hr))
+			return errorRet;
+
+		LPWSTR wstrID = NULL;
+		hr = pDevice->GetId(&wstrID);
+
+		if (!SUCCEEDED(hr))
+			return errorRet;
+
+		IPropertyStore* pStore;
+		hr = pDevice->OpenPropertyStore(STGM_READ, &pStore);
+
+		if (!SUCCEEDED(hr))
+			return errorRet;
+
+		PROPVARIANT friendlyName;
+		PropVariantInit(&friendlyName);
+		hr = pStore->GetValue(PKEY_Device_FriendlyName, &friendlyName);
+
+		if (!SUCCEEDED(hr))
+			return errorRet;
+
+		// if no options, print the device
+		// otherwise, find the selected device and set it to be default
+		if (i + 1 == option)
 		{
-			IMMDeviceCollection *pDevices;
-			// Enumerate the output devices.
-			hr = pEnum->EnumAudioEndpoints(eRender, DEVICE_STATE_ACTIVE, &pDevices);
-			if (SUCCEEDED(hr))
-			{
-				UINT count;
-				pDevices->GetCount(&count);
-				if (SUCCEEDED(hr))
-				{
-					for (UINT i = 0; i < count; i++)
-					{
-						IMMDevice *pDevice;
-						hr = pDevices->Item(i, &pDevice);
-						if (SUCCEEDED(hr))
-						{
-							LPWSTR wstrID = NULL;
-							hr = pDevice->GetId(&wstrID);
-							if (SUCCEEDED(hr))
-							{
-								IPropertyStore *pStore;
-								hr = pDevice->OpenPropertyStore(STGM_READ, &pStore);
-								if (SUCCEEDED(hr))
-								{
-									PROPVARIANT friendlyName;
-									PropVariantInit(&friendlyName);
-									hr = pStore->GetValue(PKEY_Device_FriendlyName, &friendlyName);
-									if (SUCCEEDED(hr))
-									{
-										// if no options, print the device
-										// otherwise, find the selected device and set it to be default
-										if (i+1 == option) 
-										{
-											SetDefaultAudioPlaybackDevice(wstrID);
-											printf("Audio Device %d: %ws selected\n",i+1, friendlyName.pwszVal);
-										}
-										else if (argc == 2 && wcsncmp(friendlyName.pwszVal, wstrOption, strlenOption) == 0) 
-										{
-											SetDefaultAudioPlaybackDevice(wstrID);
-											printf("Audio Device %d: %ws selected\n",i+1, friendlyName.pwszVal);
-										}
-										else
-										{
-											printf("Audio Device %d: %ws\n",i+1, friendlyName.pwszVal);
-										}
-										PropVariantClear(&friendlyName);
-									}
-									pStore->Release();
-								}
-							}
-							pDevice->Release();
-						}
-					}
-				}
-				pDevices->Release();
-			}
-			pEnum->Release();
+			SetDefaultAudioPlaybackDevice(wstrID);
+			printf("Audio Device %d: %ws selected\n", i + 1, friendlyName.pwszVal);
 		}
+		else if (argc == 2 && wcsncmp(friendlyName.pwszVal, wstrOption, strlenOption) == 0)
+		{
+			SetDefaultAudioPlaybackDevice(wstrID);
+			printf("Audio Device %d: %ws selected\n", i + 1, friendlyName.pwszVal);
+		}
+		else
+		{
+			printf("Audio Device %d: %ws\n", i + 1, friendlyName.pwszVal);
+		}
+		PropVariantClear(&friendlyName);
+		pStore->Release();
+		pDevice->Release();
 	}
+	pDevices->Release();
+	pEnum->Release();
+
 	return hr;
 }
